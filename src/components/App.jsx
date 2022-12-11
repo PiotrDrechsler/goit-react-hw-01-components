4952d88f32ac5c49397cd1aa7a7b162d651bb207
PiotrDrechsler <pdrech@gmail.com>
2022-12-11 20:14:48 +0100

    Profile feature
    Add:
    
    Profile.jsx
    html structure tags
    propTypes
    user,json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Profile } from "./Profile";
+
+import user from '../data/user.json';
+
+export const App = () => (
+  <>
+     <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  </>
+)
